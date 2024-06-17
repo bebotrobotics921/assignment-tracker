b32dbae9eda9ff6af52410e0ae75c2a8807209b4
bebotrobotics921 <jessica.tc.woo@gmail.com>
2024-06-16 21:11:27 -0700

    not working

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./assignment.module.css";
 import { TbTrash } from "react-icons/tb";
+import { IoMdCheckmarkCircle } from "react-icons/io";
 import { useState } from 'react';
 
 type Props = {
@@ -12,23 +13,29 @@ export function Assignment({ assignment, completed }: Props) {
   const [assignments, setAssignments] = useState<Props[]>([]);
   
   const handleDelete = (id: string) => {
-    
-    setAssignments(assignments.filter((assignment)=> assignment.id !== id));
-    
+    setAssignments((assignments).filter(assignment=> assignment.id !== id));
   }
 
- 
+  const toggleComplete = (id: string) => {
+    console.log(id)
+    setAssignments(assignments.map((assignment)=> 
+      assignment.id === id ? {...assignment, completed: !completed} : assignment
+    )
+    );
+  }
+
   return (
     <div className={styles.assignment}>
-      <button className={styles.checkContainer} >
-        <div >
-        </div>  
+      <button className={styles.checkContainer}  >
+        <div >  
+          <IoMdCheckmarkCircle /> 
+        </div>
       </button>
      
-      <p className={`${completed ? styles.textCompleted : ""}`}>{assignment}</p>
+      <p className={completed ? styles.textCompleted : ""}>{assignment}</p>
    
-      <button className={styles.deleteButton} onClick={()=> handleDelete} >
-        <TbTrash size={20}/>
+      <button className={styles.deleteButton}  >
+        <TbTrash size={20} onClick={()=> handleDelete(assignment)}/>
       </button>
     </div>
   );
