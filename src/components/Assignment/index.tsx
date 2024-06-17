69d0bc2c00c6bc47dae0246f314c37d11b2dad6e
bebotrobotics921 <jessica.tc.woo@gmail.com>
2024-06-16 19:25:38 -0700

    idToDelete does not work

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -12,27 +12,22 @@ export function Assignment({ assignment, completed }: Props) {
   const [assignments, setAssignments] = useState<Props[]>([]);
   
   const handleDelete = (id: string) => {
-    alert("clicked")
+    
     setAssignments(assignments.filter((assignment)=> assignment.id !== id));
     
   }
 
-  const toggleComplete = (id: string) => {
-    setAssignments(assignments.map((assignment)=> 
-      assignment.id === id ? {...assignment, completed: !completed} : assignment
-    )
-    );
-  }
+ 
   return (
     <div className={styles.assignment}>
-      <button className={styles.checkContainer} onClick={()=> toggleComplete(assignment)}>
+      <button className={styles.checkContainer} >
         <div >
         </div>  
       </button>
      
       <p className={`${completed ? styles.textCompleted : ""}`}>{assignment}</p>
    
-      <button className={styles.deleteButton} onClick={()=> handleDelete(assignment)} >
+      <button className={styles.deleteButton} onClick={()=> handleDelete} >
         <TbTrash size={20}/>
       </button>
     </div>
